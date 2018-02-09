031c68f67d16d8067200b2ea6aa5fbbed19d65f7
Alex McNerney <ajm13@me.com>
2018-02-08 21:27:02 -0800

    forgot to save Home.vue lol

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,14 +44,6 @@ export default {
         day: 'numeric'
       })
     }
-  },
-
-  filters: {
-    time(v) {
-      let h = Math.floor(v / 60)
-      let m = ('00' + v % 60).substr(-2)
-      return h + ':' + m
-    }
   }
 }
 </script>
